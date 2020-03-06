d8ff2d65b124c40918cb531de78267d5a3a5d9ce
Андрей Бондаренко <a.bondarenko@bondarenko-a-msk.local>
2020-03-06 13:58:07 +0300

    Added vimeo support to vue-gallery js

--- dist/js/vue-gallery.js
+++ dist/js/vue-gallery.js
@@ -1,36 +1,36 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('blueimp-gallery/css/blueimp-gallery.min.css'), require('blueimp-gallery/js/blueimp-gallery-fullscreen.js'), require('blueimp-gallery/js/blueimp-gallery-video.js'), require('blueimp-gallery/js/blueimp-gallery-youtube.js'), require('blueimp-gallery/js/blueimp-gallery.js')) :
-  typeof define === 'function' && define.amd ? define(['blueimp-gallery/css/blueimp-gallery.min.css', 'blueimp-gallery/js/blueimp-gallery-fullscreen.js', 'blueimp-gallery/js/blueimp-gallery-video.js', 'blueimp-gallery/js/blueimp-gallery-youtube.js', 'blueimp-gallery/js/blueimp-gallery.js'], factory) :
-  (global = global || self, global.VueGallery = factory(null, null, null, null, global.blueimp));
-}(this, function (blueimpGallery_min_css, blueimpGalleryFullscreen_js, blueimpGalleryVideo_js, blueimpGalleryYoutube_js, blueimp) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('blueimp-gallery/css/blueimp-gallery.min.css'), require('blueimp-gallery/js/blueimp-gallery-fullscreen.js'), require('blueimp-gallery/js/blueimp-gallery-video.js'), require('blueimp-gallery/js/blueimp-gallery-youtube.js'), require('blueimp-gallery/js/blueimp-gallery-vimeo.js'), require('blueimp-gallery/js/blueimp-gallery.js')) :
+  typeof define === 'function' && define.amd ? define(['blueimp-gallery/css/blueimp-gallery.min.css', 'blueimp-gallery/js/blueimp-gallery-fullscreen.js', 'blueimp-gallery/js/blueimp-gallery-video.js', 'blueimp-gallery/js/blueimp-gallery-youtube.js', 'blueimp-gallery/js/blueimp-gallery-vimeo.js', 'blueimp-gallery/js/blueimp-gallery.js'], factory) :
+  (global = global || self, global.VueGallery = factory(null, null, null, null, null, global.blueimp));
+}(this, (function (blueimpGallery_min_css, blueimpGalleryFullscreen_js, blueimpGalleryVideo_js, blueimpGalleryYoutube_js, blueimpGalleryVimeo_js, blueimp) { 'use strict';
 
-  blueimp = blueimp && blueimp.hasOwnProperty('default') ? blueimp['default'] : blueimp;
+  blueimp = blueimp && Object.prototype.hasOwnProperty.call(blueimp, 'default') ? blueimp['default'] : blueimp;
 
   //
   var script = {
     props: {
       images: {
         type: Array,
-        default: function _default() {
+        "default": function _default() {
           return [];
         }
       },
       options: {
         type: Object,
-        default: function _default() {
+        "default": function _default() {
           return {};
         }
       },
       carousel: {
         type: Boolean,
-        default: false
+        "default": false
       },
       index: {
         type: Number
       },
       id: {
         type: String,
-        default: 'blueimp-gallery'
+        "default": 'blueimp-gallery'
       }
     },
     data: function data() {
@@ -280,7 +280,7 @@
     /* style */
     const __vue_inject_styles__ = function (inject) {
       if (!inject) return
-      inject("data-v-6d2033ba_0", { source: ".blueimp-gallery>.description{position:absolute;top:30px;left:15px;color:#fff;display:none}.blueimp-gallery-controls>.description{display:block}", map: undefined, media: undefined });
+      inject("data-v-65097298_0", { source: ".blueimp-gallery>.description{position:absolute;top:30px;left:15px;color:#fff;display:none}.blueimp-gallery-controls>.description{display:block}", map: undefined, media: undefined });
 
     };
     /* scoped */
@@ -306,4 +306,4 @@
 
   return VueGallery;
 
-}));
+})));
